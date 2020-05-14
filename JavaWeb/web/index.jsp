7f79943735295b6c26eb7aa98004bfb55619e972
gy0851 <geyao_0851@163.com>
2020-05-14 11:21:35 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: geyao
+  Date: 2020/5/8
+  Time: 14:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>oh yeah</title>
+  </head>
+  <body>
+  <h1 style="color:red">asdfasdfasdf</h1>
+  </body>
+</html>
---
+++ src/com/test/Demo.java
@@ -0,0 +1,16 @@
+package com.test;
+
+
+import javax.swing.filechooser.FileSystemView;
+
+public class Demo {
+    public static void main(String[] args) {
+        StringBuffer sb = mathod();
+        System.out.println(sb);
+    }
+
+    public static StringBuffer mathod() {
+        StringBuffer sb = new StringBuffer("fuck you bitch!!");
+        return sb;
+    }
+}
